0019e1a268c4120e9e17f24e02834f30ad4e3d2e
Amonyous <amonyous@163.com>
2019-11-26 23:14:45 +0800

    Learn form MakerBean in 20191126

--- my_report.py
+++ my_report.py
@@ -1,14 +1,30 @@
-name = input('请输入您的姓名：')
-gender = input('请输入您的性别：')
-age= input('请输入您的年龄：')
-university = input('请输入您的学校：')
-print('正在生成您的简历......\n')
-import time
-time.sleep(3)
-print('*'*30)
-print('简历'.center(30,'\000')+'\n')
-print('姓名：' + '\t' + name)
-print('性别：' + '\t' + gender)
-print('年龄：' + '\t' + age)
-print('学校：' + '\t' + university)
-print('*'*30)
+# name = input('请输入您的姓名：')
+# gender = input('请输入您的性别：')
+# age= input('请输入您的年龄：')
+# university = input('请输入您的学校：')
+# print('正在生成您的简历......\n')
+# import time
+# time.sleep(3)
+# print('*'*30)
+# print('简历'.center(30,'\000')+'\n')
+# print('姓名：' + '\t' + name)
+# print('性别：' + '\t' + gender)
+# print('年龄：' + '\t' + age)
+# print('学校：' + '\t' + university)
+# print('*'*30)
+
+st = list('1948736495')
+ou = []
+ji = []
+st_length = len(st)
+i,j = 0,0
+while i < st_length:
+    if int(st[i]) % 2 != 0:
+        ou.append(st[i])
+        i = i + 1
+    else:
+        ji.append(st[i])
+        i = i + 1
+print(ou)
+print('*'*20)
+print(ji)
